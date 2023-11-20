8eac9a13b365d07869d00aac953d1f7b173a3844
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2023-11-20 23:35:12 +0200

    style City input with daisy ui

--- app/components/CityAutocomplete.tsx
+++ app/components/CityAutocomplete.tsx
@@ -1,47 +1,48 @@
-'use client'
-import { useState, useEffect } from 'react';
-import { useCombobox } from 'downshift';
+"use client";
+import { useState, useEffect } from "react";
+import { useCombobox } from "downshift";
 
 export default function CityAutocomplete() {
   const [inputItems, setInputItems] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
-  const {
-    isOpen,
-    getMenuProps,
-    getInputProps,
-    highlightedIndex,
-    getItemProps,
-  } = useCombobox({
+  const { isOpen, getMenuProps, getInputProps, highlightedIndex, getItemProps } = useCombobox({
     items: inputItems,
     onInputValueChange: ({ inputValue }) => {
-        inputValue && setInputValue(inputValue);
-    },
+      inputValue && setInputValue(inputValue);
+    }
   });
 
-  useEffect(() => {
-    if (inputValue.length > 2) {
-      fetch(`https://geocode.maps.co/search?q=${inputValue}`)
-        .then(response => response.json())
-        .then(data => {
-          setInputItems(data.map((result:any) => result.display_name));
-        });
-    }
-  }, [inputValue]);
+  useEffect(
+    () => {
+      if (inputValue.length > 2) {
+        fetch(`https://geocode.maps.co/search?q=${inputValue}`)
+          .then(response => response.json())
+          .then(data => {
+            setInputItems(data.map((result: any) => result.display_name));
+          });
+      }
+    },
+    [inputValue]
+  );
 
   return (
     <div className="w-full max-w-xs mx-auto">
-      <input {...getInputProps()} className="w-full px-3 py-2 text-gray-700 border rounded-md focus:outline-none focus:shadow-outline" />
+      <input
+        {...getInputProps()}
+        type="text"
+        placeholder="Type your city here"
+        className="input input-bordered input-primary w-full max-w-xs"
+      />
+
       {isOpen && (
-        <ul {...getMenuProps({},{suppressRefError:true})} className="absolute mt-1 w-full bg-white rounded-md shadow-lg">
+        <ul
+          {...getMenuProps({}, { suppressRefError: true })}
+          className="absolute mt-2 menu bg-base-200 w-full max-w-xs  max-h-40 overflow-hidden overflow-y-auto flex-nowrap rounded-box gap-2 py-2"
+        >
           {inputItems.map((item, index) => (
-            <li
-              style={highlightedIndex === index ? { backgroundColor: '#bde4ff' } : {}}
-              key={`${item}${index}`}
-              {...getItemProps({ item, index })}
-              className="relative p-2 cursor-pointer"
-            >
-              {item}
+            <li key={`${item}${index}`} {...getItemProps({ item, index })}>
+              <a className={`${highlightedIndex === index ? "active" : ""}`}> {item}</a>
             </li>
           ))}
         </ul>
